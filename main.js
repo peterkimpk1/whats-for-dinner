b5c532a9b24f038e4cd020e9e7a7f130b319ada9
Peter Kim <peterkim.pk1@gmail.com>
2024-04-05 12:57:18 -0400

    Created query variables, and eventlistener for button. Added function to show the selected user's meal and hide the cookpot image.

--- main.js
+++ main.js
@@ -48,3 +48,43 @@ var desserts = [
     "Eclairs"
   ];
 
+//queryvariables
+var sideInput = document.querySelector('#side');
+var mainDishInput = document.querySelector('#main-dish');
+var dessertInput = document.querySelector('#dessert');
+var entireMealInput = document.querySelector('#entire-meal');
+var letsCookButton = document.querySelector('#lets-cook');
+var cookPotImage = document.querySelector('#cookpot')
+var userShouldMakeText = document.querySelector('.you-should-make')
+var secondWrapper = document.querySelector('#second-box')
+letsCookButton.addEventListener('click', showMeals);
+
+
+function showMeals(event) {
+  event.preventDefault();
+  var randomSideDish = randomMeal(sides);
+  var randomMainDish = randomMeal(mains);
+  var randomDessertDish = randomMeal(desserts);
+  var entireMeal = [randomMainDish, randomSideDish, randomDessertDish]
+  userShouldMakeText.classList.remove('hidden');
+  cookPotImage.innerHTML = "";
+  if (sideInput.checked) {
+    cookPotImage.innerHTML = `<p class=any-dish>${randomSideDish}!</p>`
+  }
+  else if (mainDishInput.checked) {
+    cookPotImage.innerHTML = `<p class=any-dish>${randomMainDish}!</p>`
+  }
+  else if (dessertInput.checked) {
+    cookPotImage.innerHTML = `<p class=any-dish>${randomDessertDish}!</p>`
+  }
+  else if (entireMealInput.checked) {
+    cookPotImage.innerHTML = `<p class=entire-meal>${entireMeal[0]} with a side of ${entireMeal[1]} and ${entireMeal[2]} for dessert!</p>`
+  }
+}
+
+function randomMeal(meal) {
+  return meal[getRandomNumber(meal.length)];
+}
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max)
+}
